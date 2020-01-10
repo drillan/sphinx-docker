58b9545b91415469ba7f4ae27da547b06eea676c
driller <eleshis@gmail.com>
2020-01-10 12:14:24 +0900

    update test script

--- test/build.sh
+++ test/build.sh
@@ -1,2 +1,4 @@
 docker run --rm -v $PWD:/docs driller/sphinx:base sphinx-quickstart -q -p test -a auther --sep
 docker run --rm -v $PWD:/docs driller/sphinx:base make html
+docker run --rm -v $PWD:/docs driller/sphinx:base make epub
+docker run --rm -v $PWD:/docs driller/sphinx:pdf make latexpdf
