d5dbbb4aba04fdf3f081830558af325b0e72a57b
driller <eleshis@gmail.com>
2020-01-10 13:52:03 +0900

    update README

--- README.rst
+++ README.rst
@@ -16,15 +16,15 @@ Usage
 
 Create a Sphinx project::
 
-  $ docker run --rm -v /path/to/document:/docs tk0miya/sphinx:base sphinx-quickstart
+  $ docker run --rm -v /path/to/document:/docs tk0miya/sphinx sphinx-quickstart
 
 Build HTML document::
 
-  $ docker run --rm -v /path/to/document:/docs tk0miya/sphinx:base make html
+  $ docker run --rm -v /path/to/document:/docs tk0miya/sphinx make html
 
 Build EPUB document::
 
-  $ docker run --rm -v /path/to/document:/docs tk0miya/sphinx:base make epub
+  $ docker run --rm -v /path/to/document:/docs tk0miya/sphinx make epub
 
 Build PDF document::
 
@@ -32,7 +32,15 @@ Build PDF document::
 
 Build HTML document with specified version::
 
-  $ docker run --rm -v /path/to/document:/docs tk0miya/sphinx:base2.3 make html
+  $ docker run --rm -v /path/to/document:/docs tk0miya/sphinx:2.3 make html
+
+Build EPUB document with specified version::
+
+  $ docker run --rm -v /path/to/document:/docs tk0miya/sphinx:2.3 make epub
+
+Build PDF document with specified version::
+
+  $ docker run --rm -v /path/to/document:/docs tk0miya/sphinx:pdf2.3 make latexpdf
 
 Tips
 ----
--- test/build.sh
+++ test/build.sh
@@ -1,14 +1,18 @@
-rm -f Makefile make.bat
-rm -rf build source
-
 # latest
+rm -rf Makefile make.bat build source
 docker run --rm -v $PWD:/docs driller/sphinx:base sphinx-quickstart -q -p test -a auther --sep
 docker run --rm -v $PWD:/docs driller/sphinx:base make html
 docker run --rm -v $PWD:/docs driller/sphinx:base make epub
 docker run --rm -v $PWD:/docs driller/sphinx:pdf make latexpdf
 
+rm -rf Makefile make.bat build source
+docker run --rm -v $PWD:/docs driller/sphinx sphinx-quickstart -q -p test -a auther --sep
+docker run --rm -v $PWD:/docs driller/sphinx make html
+docker run --rm -v $PWD:/docs driller/sphinx make epub
+
 # 2.3
-docker run --rm -v $PWD:/docs driller/sphinx:base2.3 sphinx-quickstart -q -p test -a auther --sep
-docker run --rm -v $PWD:/docs driller/sphinx:base2.3 make html
-docker run --rm -v $PWD:/docs driller/sphinx:base2.3 make epub
+rm -rf Makefile make.bat build source
+docker run --rm -v $PWD:/docs driller/sphinx:2.3 sphinx-quickstart -q -p test -a auther --sep
+docker run --rm -v $PWD:/docs driller/sphinx:2.3 make html
+docker run --rm -v $PWD:/docs driller/sphinx:2.3 make epub
 docker run --rm -v $PWD:/docs driller/sphinx:pdf2.3 make latexpdf
