b11f446ac9b862d051d47f3ab02c0bdba43a24c7
driller <eleshis@gmail.com>
2020-01-11 14:49:13 +0900

    change test script

---
+++ test/test_unix.sh
@@ -0,0 +1,13 @@
+# latest
+rm -rf Makefile make.bat build source
+docker run --rm -v $PWD:/docs sphinxdoc/sphinx sphinx-quickstart -q -p test -a auther --sep
+docker run --rm -v $PWD:/docs sphinxdoc/sphinx make html
+docker run --rm -v $PWD:/docs sphinxdoc/sphinx make epub
+docker run --rm -v $PWD:/docs sphinxdoc/sphinx-latexpdf make latexpdf
+
+# 2.3
+rm -rf Makefile make.bat build source
+docker run --rm -v $PWD:/docs sphinxdoc/sphinx:2.3 sphinx-quickstart -q -p test -a auther --sep
+docker run --rm -v $PWD:/docs sphinxdoc/sphinx:2.3 make html
+docker run --rm -v $PWD:/docs sphinxdoc/sphinx:2.3 make epub
+docker run --rm -v $PWD:/docs sphinxdoc/sphinx-latexpdf:2.3 make latexpdf
